8e834fb1d9b898202f112ecb0b959b095d3a5ba2
vaiscode <vaistv.vp@gmail.com>
2024-02-05 22:02:15 +0530

    Emailvalidate

--- validation.js
+++ validation.js
@@ -2,7 +2,8 @@ var email=document.getElementById('email');
 var valid=document.getElementById('validation');
 
 function validate(){
-    var regexp = /^([a-zA-z0-9.-]+)@([a-zA-Z0-9]+)\.([a-z]{2,3})(\.[a-zA-Z]{2,3})?$ /
+    var regexp = /^([a-zA-Z0-9.-]+)@([a-zA-Z0-9\-]+)\.([a-z]{2,3})(\.[a-zA-Z]{2,3})?$/
+    
     if(regexp.test(email.value)){
         valid.innerText="Email is Valid";
         return true;
